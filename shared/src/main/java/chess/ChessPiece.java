773b8853777860103a728d48e196c26b4b614765
SethMortensen <sethrmortensen@gmail.com>
2026-09-10 10:30:43 -0600

    Added MoveStrategy superclass with KnightMoveStrategy subclass to implement knight movement logic.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
@@ -12,7 +13,7 @@ import java.util.Objects;
 public class ChessPiece {
 
     private final ChessGame.TeamColor pieceColor;
-    private ChessPiece.PieceType type;
+    private final ChessPiece.PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
@@ -59,6 +60,30 @@ public class ChessPiece {
         return Objects.hash(pieceColor, type);
     }
 
+    interface MoveStrategy {
+        Collection<ChessMove> getValidMoves(ChessPosition position, ChessBoard board);
+    }
+
+    static class KnightMoveStrategy implements ChessPiece.MoveStrategy {
+        @Override
+        public Collection<ChessMove> getValidMoves(ChessPosition position, ChessBoard board) {
+            Collection<ChessMove> moves = new ArrayList<>();
+            int[][] offsets = { {2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2} };
+
+            for (int[] offset : offsets) {
+                ChessPosition target = new ChessPosition(position.getRow() + offset[0], position.getColumn() + offset[1]);
+                if (target.getColumn() <= 8 && target.getColumn() >= 1 && target.getRow() <= 8 && target.getRow() >= 1) {
+                    if(board.getPiece(target) == null) {
+                        moves.add(new ChessMove(position, target, null));
+                    } else if (board.getPiece(position).pieceColor != board.getPiece(target).pieceColor) {
+                        moves.add(new ChessMove(position, target, null));
+                    }
+                }
+            }
+            return moves;
+        }
+    }
+
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -67,6 +92,10 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        if (board.getPiece(myPosition).type == ChessPiece.PieceType.KNIGHT) {
+            MoveStrategy strategy = new KnightMoveStrategy();
+            return strategy.getValidMoves(myPosition, board);
+        }
+        return null;
     }
 }
